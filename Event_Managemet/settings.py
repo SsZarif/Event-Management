8c13d2f8171f1d3c5dfabefb73a0d3be7aaeb699
Plasmaa <zarifpious11@gmail.com>
2025-01-29 19:35:13 +0600

    ready for deployment

--- Event_Managemet/settings.py
+++ Event_Managemet/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -69,15 +70,24 @@ WSGI_APPLICATION = 'Event_Managemet.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 # For PostgreSQL
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'Event_Managemet',
+#         'USER': 'postgres',
+#         'PASSWORD': '1234',
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+#     }
+# }
+
+# Replace the SQLite DATABASES configuration with PostgreSQL:
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Event_Managemet',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://event_manager_9t2f_user:ZWcBvghWjonk92LsR54CdikPqIJWPlWR@dpg-cud2oii3esus73fu0it0-a.oregon-postgres.render.com/event_manager_9t2f',
+        conn_max_age=600
+    )
 }
 
 # For SQLite
